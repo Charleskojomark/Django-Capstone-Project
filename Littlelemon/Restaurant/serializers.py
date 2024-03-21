8d991a04b6dc6a04ef794693f5cf32ce1bed6ed4
Charleskojomark <markcharleskojo@gmail.com>
2024-03-21 14:23:12 +0100

    added serializers

---
+++ Littlelemon/Restaurant/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Booking, Menu
+from django.contrib.auth.models import User, Group
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = '__all__'
--- Littlelemon/Restaurant/views.py
+++ Littlelemon/Restaurant/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Menu, Booking
+from .serializers import MenuSerializer
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
